dbad08de6abb7ab9b3f336b73f003a1c18f2b9fb
A-Selian <117290158+A-Selian@users.noreply.github.com>
2025-05-24 21:28:33 +0700

    Pertemuan 2: luasPpanjang
    Menambahkan penjelasan kegunaan program

--- Pertemuan 2/luasPpanjang.cpp
+++ Pertemuan 2/luasPpanjang.cpp
@@ -5,14 +5,15 @@ int main () {
 
     float luas, p, l;
 
+    // Program untuk menghitung luas persegi panjang
     cout << "Masukkan panjang : ";
     cin >> p;
 
     cout << "Masukkan lebar : ";
     cin >> l;
 
-    luas = p * l;
+    luas = p * l; // menjelaskan variabel luas didapat dari nilai p dikali l
     cout << "Luas = " << luas << endl;
 
-    return 0;
+    return 0; // Mengembalikan nilai 0 menandakan program selesai dengan baik
 }
