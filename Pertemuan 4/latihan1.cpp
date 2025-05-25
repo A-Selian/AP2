973c940319a68050ffa932aea2c34296d2183e25
A-Selian <117290158+A-Selian@users.noreply.github.com>
2025-05-25 12:10:16 +0700

    Pertemuan 4: latihan1
    Menjelaskan kegunaan program dan menjelaskan fungsi toupper

--- Pertemuan 4/latihan1.cpp
+++ Pertemuan 4/latihan1.cpp
@@ -6,12 +6,12 @@ int main(){
     int i;
 
     system("CLS");
-
+    // Program untuk mengubah kalimat menjadi huruf kapital
     cout << "Masukkan kalimat : ";
     getline(cin, kalimat);
  
-    for(i = 0; i < kalimat.length(); i++) {
-        kalimat[i] = toupper(kalimat[i]);
+    for(i = 0; i < kalimat.length(); i++) { // for digunakan untuk menyatakan kondisi dengan syarat
+        kalimat[i] = toupper(kalimat[i]); // toupper merupakan fungsi yang digunakan untuk mengubah karakter menjadi huruf kapital
     }
 
     cout << "Kalimat dalam huruf kapital : " << kalimat << endl;
