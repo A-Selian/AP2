2cecdff09f91a1db8c833f77c72d9202b9113539
A-Selian <117290158+A-Selian@users.noreply.github.com>
2025-05-25 12:24:13 +0700

    Pertemuan 4: latihan3
    Menjelaskan kegunaan program

--- Pertemuan 4/latihan3.cpp
+++ Pertemuan 4/latihan3.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+    // Program untuk mengecek apakah input kedua sesuai dengan pola yang ditentukan
     string password, sandi, expected = "";
 
     cout << "Masukkan kata sandi : ";
@@ -9,14 +10,15 @@ int main() {
 
     cout << "Masukkan kata : ";
     cin >> sandi;
-
+    
     for (int i = 0; i < password.length(); i++) {
         expected += password.substr(0, i + 1);
     }
-
+    /* Pola yang benar adalah: 112123123412345......nnnnnnn */
     if (expected == sandi) {
         cout << "Tulisan agen benar" << endl;
-    } else {
+    } 
+    else {
         cout << "Tulisan agen salah" << endl;
     }
 
