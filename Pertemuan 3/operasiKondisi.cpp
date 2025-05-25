0b71d9c4ad05ee2a0b370419b97f995f10b90635
A-Selian <117290158+A-Selian@users.noreply.github.com>
2025-05-25 11:56:03 +0700

    Pertemuan 3: OperasiKondisi
    Menjelaskan operator-operator kondisi, meliputi if statement, switch, dan ternary operator

--- Pertemuan 3/operasiKondisi.cpp
+++ Pertemuan 3/operasiKondisi.cpp
@@ -10,41 +10,52 @@ int main () {
     cout << "Masukkan nilai : ";
     cin >> nilai;
 
-    // if statement
+    // if statement adalah pernyataan yang digunakan untuk menguji suatu kondisi
+
+    /* if digunakan untuk menyatakan kondisi pertama
+    jika kondisi pertama benar, maka akan mengeksekusi blok kode berikut */    
     if (nilai <= 65) {
         cout << "Anda tidak lulus!" << endl;
     }
-
-    // if - else statement
+    
+    // if - else statement adalah pernyataan yang paling dasar
     if (nilai <= 65) {
         cout << "Anda tidak lulus!" << endl;
-    } else {
+    } 
+    // else statement adalah pernyataan yang digunakan untuk menangani kondisi jika semua kondisi sebelumnya tidak terpenuhi
+    else {
         cout << "Anda lulus !" << endl;
     }
 
-    // if - else - if statement
+    // if - else - if statement adalah pernyataan yang digunakan unruk menguji beberapa kondisi
     if (nilai == 100) {
         cout << "Anda Keceh" << endl;
-    } else if (nilai <= 65) {
+    } 
+    // else if statement adalah pernyataan yang digunakan untuk menguji kondisi lain jika kondisi sebelumnya tidak terpenuhi
+    else if (nilai <= 65) {
         cout << "Anda tidak lulus" << endl;
-    } else {
+    } 
+    else {
         cout << "Anda lulus" << endl;
     }
 
-    // nested if
+    // nested if (if di dalam if)
     if (nilai <= 65) {
         cout << "Anda tidak lulus" << endl;
-    } else {
+    } 
+    else {
         if (nilai == 100) {
             cout << "Anda lulus dan Anda Hebat" << endl;
-        } else {
+        } 
+        else {
             cout << "Anda lulus" << endl;
         }
     }
 
-    // switch case
-    switch (nilai) {
-        case 1:
+    /* switch case merupakan pernyataan yang digunakan untuk
+    memilih satu dari beberapa blok kode yang akan dieksekusi berdasarkan nilai dari suatu variabel atau ekspresi. */
+    switch (nilai) { //switch adalah ekspresi yang mrnyatakan nilai variabel mana akan digunakan sebagai patokan
+        case 1: // case adalah perintah yang akan dieksekusi jika nilai variabel sama dengan nilai case
             cout << "Senin" << endl;
             break;
         case 2:
@@ -65,12 +76,12 @@ int main () {
         case 7:
             cout << "Minggu" << endl;
             break;
-        default selain dari pernyataan yg ada
+        default // Perintah berikut akan dieksekusi jika nilai variabel berberda dari pernyataan yang ada
             cout << "Inputan tidak valid" << endl;
             break;
     }
 
-    // switch range
+    // switch range adalah switch yang dapat digunakan untuk menguji case dalam range nilai tertentu
     switch (nilai) {
         case 85 ... 100 : cout << "A" << endl; break;
         case 80 ... 84 : cout << "B+" << endl; break;
@@ -81,14 +92,15 @@ int main () {
         default: cout << "E" << endl;
     }
 
-    // Ternary Operator
+    // Ternary Operator adalah operator yang memungkinkan kita untuk menulis if-else dalam satu baris
     if (nilai % 2 == 0) {
         cout << "Genap sih" << endl;
-    } else {
-        cout << "Ganjil sih" << endl;
+    } 
+    else {
+        cout << "Fix, ini Ganjil" << endl;
     }
 
-    a > 5 ? 9 : 6
+    a > 5 ? 9 : 6 // contoh ternary operator
     string checkNum = (nilai % 2 == 0) ? "Genap" : "Ganjil";
     cout << nilai << " tuh Bilangan " << checkNum << " sih " << endl;
 }
